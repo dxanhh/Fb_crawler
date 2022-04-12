de670e2d23d5065009f2a590f1302e76c1c22234
Hyprnx <toducanh2001@gmail.com>
2022-04-12 20:06:09 +0700

    remove unused file

--- to_be_deleted/count_uid.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 21 08:59:58 2022
-
-@author: Administrator
-"""
-f = open("BO_UID.txt","r",encoding = "utf-8")
-lines = f.readlines()
-print(len(lines))
--- to_be_deleted/crawl_data.bat
+++
@@ -1,8 +0,0 @@
-set root=D:\Users\Administrator\Anaconda3_6
-
-call %root%\Scripts\activate.bat %root%
-
-cd C:\batfile\set up server\crawl facebook
-autopep8 -i crawl_thongtin_json_5.py
-call python crawl_thongtin_json_5.py
-PAUSE
